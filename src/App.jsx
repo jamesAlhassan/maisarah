024cc86d1346f5f2753b0ad7a8d393a26def1301
James Alhassan <jamesalhassan052@gmail.com>
2023-05-30 20:53:34 +0000

    Add DateTime component setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
 import Dashboard from "./assets/components/Dashboard";
+import DateTimeInfo from "./assets/components/DateTimeInfo";
 import "./App.css";
 
 const App = () => {
-  return <div>{/* <Dashboard /> */}</div>;
+  return (
+    <div>
+      <DateTimeInfo />
+    </div>
+  );
 };
 export default App;
