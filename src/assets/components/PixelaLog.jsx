63503ddd18d650b86823b367b94a98f6e20f255e
James Alhassan <jamesalhassan052@gmail.com>
2023-05-31 14:35:17 +0000

    Pixela Form

--- src/assets/components/PixelaLog.jsx
+++ src/assets/components/PixelaLog.jsx
@@ -1,6 +1,35 @@
-import { useState } from "react";
 
 const PixelaLog = () => {
-  return <h1>Pixela</h1>;
+  return (
+<div>
+    <form className='form' onSubmit={handleSubmit}>
+        <h4>Form Data API</h4>
+        {/* name */}
+        <div className='form-row'>
+          <label htmlFor='name' className='form-label'>
+            name
+          </label>
+          <input type='text' className='form-input' id='name' name='name' />
+        </div>
+        {/* email */}
+        <div className='form-row'>
+          <label htmlFor='email' className='form-label'>
+            Email
+          </label>
+          <input type='email' className='form-input' id='email' name='email' />
+        </div>
+        {/* email */}
+        <div className='form-row'>
+          <label htmlFor='password' className='form-label'>
+            Password
+          </label>
+          <input
+            type='password'
+            className='form-input'
+            id='password'
+            name='password'
+          />
+</div>
+  )
 };
 export default PixelaLog;
