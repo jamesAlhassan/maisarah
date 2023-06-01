22ce320fb1c8c895ac1d8be3a8b35236b0670088
James Alhassan <jamesalhassan052@gmail.com>
2023-06-01 12:47:50 +0000

    Update Error.jsx

--- src/assets/components/Error.jsx
+++ src/assets/components/Error.jsx
@@ -1,4 +1,14 @@
+import styled from "styled-components";
+import { Link } from "react-router-dom";
+
 const Error = () => {
-  return <div>Error</div>;
+  return (
+    <Wrapper>
+      <div>
+        <h1>404</h1>
+        <h3>sorry, the pages does not exist!!!</h3>
+      </div>
+    </Wrapper>
+  );
 };
 export default Error;
