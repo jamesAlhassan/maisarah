ac38b1d9e85e1cc89f276b865a1e46c4393bb294
James Alhassan <jamesalhassan052@gmail.com>
2023-06-01 13:26:45 +0000

    Update Login.jsx

--- src/assets/components/Login.jsx
+++ src/assets/components/Login.jsx
@@ -1,4 +1,14 @@
+import styled from "styled-components";
+import loginImg from "../images/login-img.svg";
 const Login = () => {
-  return <div>Login</div>;
+  return (
+    <Wrapper>
+      <div className='container'>
+        <img src={loginImg} alt='user login' />
+        <h1>employees login</h1>
+        <button className='btn'>login</button>
+      </div>
+    </Wrapper>
+  );
 };
 export default Login;
