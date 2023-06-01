494f25d11ccf76e5acb47aae72585756512c2742
James Alhassan <jamesalhassan052@gmail.com>
2023-06-01 12:51:57 +0000

    Update Error.jsx

--- src/assets/components/Error.jsx
+++ src/assets/components/Error.jsx
@@ -7,8 +7,26 @@ const Error = () => {
       <div>
         <h1>404</h1>
         <h3>sorry, the pages does not exist!!!</h3>
+        <Link to='/' className='btn'>
+          back home
+        </Link>
       </div>
     </Wrapper>
   );
 };
+
+const Wrapper = styled.section`
+  min-height: 100vh;
+  display: grid;
+  place-items: center;
+  background: var(--clr-primary-10);
+  text-align: center;
+  h1 {
+    font-size: 10rem;
+  }
+  h3 {
+    color: var(--clr-grey-3);
+    margin-bottom: 1.5rem;
+  }
+`;
 export default Error;
