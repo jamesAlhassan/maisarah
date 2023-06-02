f2d43dfde4dd37d901a0204cd5a14933a3336c84
James Alhassan <jamesalhassan052@gmail.com>
2023-06-02 09:52:15 +0000

    Add AuthWrapper.jsx

--- src/assets/components/AuthWrapper.jsx
+++ src/assets/components/AuthWrapper.jsx
@@ -21,4 +21,14 @@ const AuthWrapper = ({ children }) => {
 
   return <>{children}</>;
 };
+
+const Wrapper = styled.section`
+  min-height: 100vh;
+  display: grid;
+  place-items: center;
+  img {
+    width: 150px;
+  }
+`;
+
 export default AuthWrapper;
