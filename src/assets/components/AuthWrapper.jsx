13acde14f98661ccec158f6e036b86eaf152b8d1
James Alhassan <jamesalhassan052@gmail.com>
2023-06-02 09:40:21 +0000

    Add AuthAWraapper

---
+++ src/assets/components/AuthWrapper.jsx
@@ -0,0 +1,6 @@
+import { useAuth0 } from "@auth0/auth0-react";
+import styled from "styled-components";
+const AuthWrapper = () => {
+  return <div>AuthWrapper</div>;
+};
+export default AuthWrapper;
