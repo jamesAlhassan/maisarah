cac17fa0a83efa7d88df30b58a2c74032eb670c1
James Alhassan <jamesalhassan052@gmail.com>
2023-06-02 09:25:00 +0000

    Add PrivateRoute

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Dashboard from "./assets/components/Dashboard";
+import PrivateRoute from "./assets/components/PrivateRoute";
 import Login from "./assets/components/Login";
 import Error from "./assets/components/Error";
 import "./App.css";
@@ -27,7 +28,14 @@ const App = () => {
   return (
     <Router>
       <Routes>
-        <Route path='/' element={<Dashboard />} />
+        <Route
+          path='/'
+          element={
+            <PrivateRoute>
+              <Dashboard />
+            </PrivateRoute>
+          }
+        />
         <Route path='/login' element={<Login />} />
         <Route path='*' element={<Error />} />
       </Routes>
