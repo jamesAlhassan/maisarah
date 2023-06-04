175a2d53907f375443c8a97823e8b1c5ff493203
James Alhassan <jamesalhassan052@gmail.com>
2023-06-04 13:55:17 +0000

    Updated

--- src/assets/components/PixelaFuncs.jsx
+++ src/assets/components/PixelaFuncs.jsx
@@ -2,8 +2,8 @@ const pixelaEndPoint = "https://pixe.la/v1/users";
 
 const date = new Date();
 
-let day = date.getDate();
-let month = date.getMonth() + 1;
+let day = String(date.getDate()).padStart(2, "0");
+let month = String(date.getMonth() + 1).padStart(2, "0");
 let year = date.getFullYear();
 let currentDate = `${year}${month}${day}`;
 
@@ -41,12 +41,18 @@ const request = (tk, username) => {
                 url: `https://pixe.la/v1/users/${username}/graphs/graph1/`,
                 data: {
                   date: `${currentDate}`,
-                  quantity: "5",
+                  quantity: "10",
                 },
                 headers: {
                   "X-USER-TOKEN": tk,
                 },
-              });
+              })
+                .then((res) => {
+                  if (res.status === 200) {
+                    alert(`Attendance Successfully Logged!!!`);
+                  }
+                })
+                .catch((err) => console.log(err));
             }
           })
           .catch((err) => console.log(err));
