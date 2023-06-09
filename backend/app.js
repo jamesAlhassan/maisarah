5cfa5c138b51bc97f8ceee89b1fcf1be59c8d85f
James Alhassan <jamesalhassan052@gmail.com>
2023-06-09 18:10:09 +0000

    Update

--- backend/app.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const app = express();
-app.get("/", (req, res) => {
-  res.send("Task....");
-});
-const port = 3000;
-
-app.listen(port, console.log(`Listening on Port ${port}`));
--- backend/routes/task.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-
-router.route("/").get((req, res) => {
-  res.send("get all tasks");
-});
-
-module.exports = router;
