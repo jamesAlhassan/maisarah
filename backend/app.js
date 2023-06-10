6d19f7410c3a8988b5dcf9e820b5fcc0817255a8
James Alhassan <jamesalhassan052@gmail.com>
2023-06-10 05:07:15 +0000

    Add mongodb setup

--- backend/app.js
+++ backend/app.js
@@ -1,8 +1,30 @@
+const { json } = require("express");
 const express = require("express");
 const app = express();
-app.get("/", (req, res) => {
-  res.send("Task....");
+const dbConnect = require("./db/connect");
+require("dotenv").config();
+
+app.use(json());
+
+app.get("/api/v1/users", (req, res) => {
+  res.send("All users");
+});
+
+app.get("/api/v1/users/:id", (req, res) => {
+  res.send(" user");
+});
+
+app.post("/api/v1/users", (req, res) => {
+  res.send("post users");
 });
 const port = 3000;
 
-app.listen(port, console.log(`Listening on Port ${port}`));
+const start = async () => {
+  try {
+    await dbConnect(process.env.MONGO_URI);
+    app.listen(port, console.log(`Listening on Port ${port}`));
+  } catch (error) {
+    console.log(error);
+  }
+};
+start();
